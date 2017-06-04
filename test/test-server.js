385d4709e47d4e5de7e2af6b2cae4e08108f9bf0
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-06-03 17:19:10 -0700

    finish test layout

---
+++ test/test-server.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const {app, runServer, closeServer} = require('../server');
+const {DATABASE} = require('../config');
+const knex = require('knex')(DATABASE);
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('class list with students in the class with grades', ()=>{
+  before(()=>runServer());
+  after(()=>{return knex.destroy().then(closeServer);});
+  
+  ////////////////////////////////////////////////////////////////
+  /////////          Testing Student List            ////////////
+  //////////////////////////////////////////////////////////////
+  describe('Testing the student list',function(){
+    describe('testing get and post endpoint skeleton (student)',function(){
+      
+    });
+    describe('Testing the database',function(){
+      afterEach(()=>{
+        return knex('studentlist').del().catch(err=>{
+          console.error('ERROR:',err);
+        });
+      });
+      describe('Get Tests',function(){
+
+      });
+      describe('Post Test',function(){
+
+      });
+      describe('Put Test',function(){
+
+      });
+      describe('Delete Test',function(){
+
+      });
+    });
+  });
+  ////////////////////////////////////////////////////////////////
+  /////////          Testing Class List            //////////////
+  //////////////////////////////////////////////////////////////
+  describe('Testing the student list',function(){
+    describe('testing get and post endpoint skeleton (student)',function(){
+      
+    });
+    describe('Testing the database',function(){
+      afterEach(()=>{
+        return knex('studentlist').del().catch(err=>{
+          console.error('ERROR:',err);
+        });
+      });
+      describe('Get Tests',function(){
+
+      });
+      describe('Post Test',function(){
+
+      });
+      describe('Put Test',function(){
+
+      });
+      describe('Delete Test',function(){
+
+      });
+    });
+  });
+});
